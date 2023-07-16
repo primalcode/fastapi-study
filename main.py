3da4e11a3dec39899d82ee953bee3d1cf3f721a0
yoshima <yoshima.fukuda@codeberry.co.jp>
2023-07-16 09:56:00 +0900

    add query-param optional pattern

--- main.py
+++ main.py
@@ -1,23 +1,9 @@
+from typing import Optional
 from fastapi import FastAPI
 
 app = FastAPI()
 
 
-@app.get("/")
-async def root():
-    return {"message": "Hello World"}
-
-
-@app.get("/hello/{name}")
-async def say_hello(name: str):
-    return {"message": f"Hello {name}"}
-
-
-@app.get("/sample")
-async def sample():
-    return {"sample"}
-
-
 @app.get("/query-param")
-async def query_param(param1: str = 'default param', param2: int = 1):
+async def query_param(param1: Optional[str] = None, param2: int = 1):
     return {"message": f"param1 : {param1}, param2 : {param2}"}
