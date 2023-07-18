de10e3d81c7aa8d7c3ee2b9562ab746ef6aee583
yoshima <yoshima.fukuda@codeberry.co.jp>
2023-07-18 15:34:56 +0900

    post追加

--- main.py
+++ main.py
@@ -1,10 +1,25 @@
 from typing import Optional
 from fastapi import FastAPI
+from pydantic import BaseModel
 
 app = FastAPI()
+
+
+class Item(BaseModel):
+    name: str
+    description: Optional[str] = None
+    price: int
+    tax: Optional[float] = None
 
 
 @app.get("/query-param")
 async def query_param(param1: Optional[str] = None, param2: int = 1):
     return {"message": f"param1 : {param1}, param2 : {param2}"}
+
+
 # 単純にparam1を返却するとnull，上記のように返却するとNoneとなる
+
+
+@app.post("/item/")
+async def create_item(item: Item):
+    return item
