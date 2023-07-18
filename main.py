6b6c935078fd5fed838d5a0ca697607335db6614
yoshima <yoshima.fukuda@codeberry.co.jp>
2023-07-18 15:44:36 +0900

    ネストのあるリクエストの処理

--- main.py
+++ main.py
@@ -1,8 +1,13 @@
-from typing import Optional
+from typing import Optional, List
 from fastapi import FastAPI
 from pydantic import BaseModel
 
 app = FastAPI()
+
+
+class ShopInfo(BaseModel):
+    name: str
+    location: str
 
 
 class Item(BaseModel):
@@ -12,14 +17,17 @@ class Item(BaseModel):
     tax: Optional[float] = None
 
 
+class Data(BaseModel):
+    shop_info: Optional[ShopInfo]
+    items: List[Item]
+
+
 @app.get("/query-param")
 async def query_param(param1: Optional[str] = None, param2: int = 1):
     return {"message": f"param1 : {param1}, param2 : {param2}"}
-
-
 # 単純にparam1を返却するとnull，上記のように返却するとNoneとなる
 
 
-@app.post("/item/")
-async def create_item(item: Item):
-    return item
+@app.post("/")
+async def index(data: Data):
+    return {"data": data}
