19fe38fe4ac0993e56eca16ab0e626f6b96e7f08
yoshima <yoshima.fukuda@codeberry.co.jp>
2023-07-23 17:30:03 +0900

    logicをserviceに移動

--- app/endpoints/hero.py
+++ app/endpoints/hero.py
@@ -1,85 +1,38 @@
 from typing import List
 
 from fastapi import APIRouter
-from sqlmodel import create_engine, Session, select, update
 
 from ..models.heroes import Hero
+from ..services import hero_service
 
 router = APIRouter()
-
-# localhostと書くと繋がらない
-engine = create_engine('mysql://yoshima:yoshima@127.0.0.1:3306/study_fastapi_db')
 
 
 @router.get("/", response_model=List[Hero])
 async def get_heroes():
-    with Session(engine) as session:
-        stmt = select(Hero)
-        heroes = session.exec(stmt).all()
-        return heroes
+    return hero_service.get_all_hero()
 
 
 @router.get("/{hero_id}", response_model=Hero)
 async def get_hero_by_id(hero_id: int):
-    with Session(engine) as session:
-        stmt = select(Hero).where(Hero.id == hero_id)
-        results = session.exec(stmt).one()
-    return results
+    return hero_service.get_by_id(hero_id)
 
 
 @router.put("/{hero_id}")
 async def update_hero(hero_id: int):
-    with Session(engine) as session:
-        stmt = select(Hero).where(Hero.id == hero_id)
-        result = session.exec(stmt).one()
-        result.name = "updated name"
-        session.add(result)
-        session.commit()
-        session.refresh(result)
+    hero_service.update_hero(hero_id)
 
 
 @router.post("/")
 async def create_hero(hero: Hero):
-    with Session(engine) as session:
-        print(f"追加前ID：{hero.id}")
-        session.add(hero)
-        print(f"追加後ID：{hero.id}")
-
-        print(f"コミット前ID：{hero.id}")
-        session.commit()
-        print(f"コミット後ID：{hero.id}")
-
-        # コミット後でIDは最新の状態になっているので，不要な気がする．．．
-        print(f"反映前ID：{hero.id}")
-        session.refresh(hero)
-        print(f"反映後ID：{hero.id}")
-
-    return {"success"}
+    hero_service.create_hero(hero)
 
 
 @router.put("/")
 async def bulk_update():
-    with Session(engine) as session:
-        heroes = session.query(Hero).filter(Hero.name == "yoshima").all()
-
-        session.execute(
-            update(Hero).where(Hero.name == "yoshima").values(age=45)
-        )
-        session.commit()
-
-        for hero in heroes:
-            session.refresh(hero)
-    return {"success"}
+    hero_service.bulk_update()
 
 
 @router.delete("/{hero_id}")
-async def delete(hero_id:int):
-    with Session(engine) as session:
-        statement = select(Hero).where(Hero.id == hero_id)
-        result = session.exec(statement).one()
-
-        session.delete(result)
-        session.commit()
-    return {"deleted"}
-
-
+async def delete(hero_id: int):
+    hero_service.delete(hero_id)
