04162c7081473df9dd3216be41a0229c35dc95a3
yoshima <yoshima.fukuda@codeberry.co.jp>
2023-07-25 19:30:22 +0900

    最低限の認証処理を作成 またトークンのチェックはしていない

--- app/endpoints/hero.py
+++ app/endpoints/hero.py
@@ -1,17 +1,37 @@
 from typing import List
 
-from fastapi import APIRouter, Depends
+from fastapi import APIRouter, Depends, HTTPException
+from fastapi.security import OAuth2PasswordBearer
+from jose import jwt
+from jose.exceptions import JWTError
 
+from app.auth.oauth2 import config
 from ..models.heroes import Hero
 from ..services import hero_service
-from app.auth.oauth2 import oauth2_scheme
-
 
 router = APIRouter()
 
+oauth2_scheme = OAuth2PasswordBearer(tokenUrl="token")
+
+
+async def get_current_user(token: str = Depends(oauth2_scheme)):
+    credentials_exception = HTTPException(
+        status_code=401,
+        detail="Could not validate credentials",
+        headers={"WWW-Authenticate": "Bearer"},
+    )
+    try:
+        payload = jwt.decode(token, config('GOOGLE_CLIENT_SECRET'), algorithms=["HS256"])
+        username: str = payload.get("sub")
+        if username is None:
+            raise credentials_exception
+    except JWTError:
+        raise credentials_exception
+    return username
+
 
 @router.get("/", name="list_heroes", response_model=List[Hero])
-async def get_heroes(token: str = Depends(oauth2_scheme)):
+async def get_heroes(token: str = Depends(get_current_user)):
     return hero_service.get_all_hero()
 
 
--- app/main.py
+++ app/main.py
@@ -1,10 +1,10 @@
-from authlib.integrations.starlette_client import OAuthError
-from fastapi import FastAPI, HTTPException
+from fastapi import FastAPI, HTTPException, Depends
 from starlette.middleware.sessions import SessionMiddleware
 from starlette.requests import Request
 from starlette.responses import RedirectResponse
 
 from app.auth.oauth2 import oauth
+from app.auth.oauth2 import oauth2_scheme
 from .api import api_router
 
 app = FastAPI()
@@ -15,32 +15,18 @@ app.add_middleware(SessionMiddleware, secret_key="your-secret-key")
 
 @app.get("/auth/login/google/authorized", name="authorize")
 async def authorize(request: Request):
-    print("###### START authorize")
+    code = request.query_params.get("code")
+    redirect_uri = str(request.url_for('authorize'))
+    google = oauth.create_client('google')
+    token = await google.fetch_access_token(code=code, redirect_uri=redirect_uri)
 
-    print(request.query_params)
+    print(f"Token: {token}")
 
-    # authorize時のセッション値を出力
-    print(f"Session at authorize: {request.session.items()}")
-
-    token = request.query_params.get("code")
     if not token:
         raise HTTPException(status_code=400, detail="Google OAuth code not returned")
 
-    # 正確にはこれはアクセストークンである
-    token = await oauth.google.authorize_access_token(request)
-
-    if not token:
-        raise HTTPException(status_code=400, detail="Invalid token")
-
-    # Add the access token to the session for later use
     request.session['token'] = dict(token)
 
-    # Use the token to get user info
-    user_info = await oauth.google.userinfo(token=token)
-
-    print(f"User info: {user_info}")  # この行を追加
-
-    # 認証に成功したら、ユーザーを /heroes/ にリダイレクトします。
     return RedirectResponse(url=request.app.url_path_for("list_heroes"))
 
 
@@ -48,8 +34,10 @@ async def authorize(request: Request):
 async def login(request: Request):
     redirect_uri = request.url_for('authorize')
     response = await oauth.google.authorize_redirect(request, redirect_uri)
-
-    # 追加: ログイン時のセッション値を出力
-    print(f"Session after login: {request.session.items()}")
-
     return response
+
+
+@app.get("/protected")
+async def protected(token: str = Depends(oauth2_scheme)):
+    print(f"Received token: {token}")
+    return {"message": "This is a protected endpoint."}
